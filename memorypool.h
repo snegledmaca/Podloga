c924d6eda410ecb411e34a68ff99165551e154aa
snegledmaca <snegledmaca@yahoo.com>
2012-11-11 23:28:54 +0100

    Dodao sam upravljanje memorijom preko zajedničkog prostora.
    Napravio sam nešto što se zove "memory pool". U biti na početku programa se zatraži sva memorija koja će se koristiti. U biti deklarira jedna velika "char[]" varijabla. Zatim se po potrebi daje taj prostor klasama prilikom njihovog stvaranja. Na taj način "new" ne alocira novu memoriju te se ne treba koristiti "delete". Jer se na kraju programa automatski taj prostor uništi.
    
    Sve klase koji misle koristiti taj prostor trebaju naslijediti ovu jednu koja je prepisala "new" operator da se postigne to da se koristi taj zajednički prostor.

---
+++ memorypool.h
@@ -0,0 +1,127 @@
+#ifndef MEMORYPOOL_H_INCLUDED
+#define MEMORYPOOL_H_INCLUDED
+
+//========================================================================
+// MemoryPool - dependancies
+//========================================================================
+
+//========================================================================
+// MemoryPool - definitions
+//========================================================================
+
+    #define MEGABYTE ( 1024 * 1024 )
+    #define POOLSIZE ( 1 * MEGABYTE )
+    #define ALIGNMENT sizeof(void *)
+    #define SIZE long unsigned int
+
+//========================================================================
+
+    class MemoryPool;
+
+//========================================================================
+// MemoryPool - implementations
+//========================================================================
+
+    class MemoryPool {
+
+    //===================================================================
+
+        private:
+
+            char  memory[POOLSIZE]; // Static raw memory
+            char* memory_position; // First free byte
+
+    //===================================================================
+
+        public:
+
+            MemoryPool() {
+                this->init();
+            }
+
+            ~MemoryPool() {
+                this->clear();
+            }
+
+    //===================================================================
+
+        private:
+
+            char* align( char* position ) {
+                SIZE mask = ALIGNMENT - 1;
+                SIZE relevant = (SIZE) position & mask;
+                SIZE offset = ( ALIGNMENT - relevant ) & mask;
+
+                // Basically it's ( ALIGNMENT - ( (SIZE) position % ALIGNMENT) ) % ALIGNMENT;
+
+                return position + offset;
+            }
+
+        public:
+
+            void* allocate( SIZE num_bytes ) {
+                char* bytes_start = align( memory_position );
+
+                if ( bytes_start + num_bytes >
+                    memory + sizeof( memory ) ) return NULL;
+
+                for ( char* i = bytes_start;
+                      i < bytes_start + num_bytes;
+                      *i = 0, i++ ) {}
+
+                memory_position = bytes_start + num_bytes;
+
+                return bytes_start;
+            }
+
+    //===================================================================
+
+        private:
+
+            void init() {
+                memory_position = align( memory );
+
+                for ( GLuint i = 0; i < POOLSIZE; i++ ) {
+                    memory[i] = 0; }
+            }
+
+            void clear() {
+                init();
+            }
+
+    //===================================================================
+
+    };
+
+    class FromPool {
+
+    	protected:
+
+            static MemoryPool* pool;
+
+    	//==============================================================
+
+    	public:
+
+    		FromPool( void ) {}
+    		~FromPool( void ) {}
+
+    	//==============================================================
+
+        public:
+
+            void* operator new ( SIZE num_bytes ) {
+                return FromPool::pool->allocate( num_bytes );
+            }
+
+            void operator delete( void * ) {}
+
+    	//==============================================================
+
+    };
+
+    MemoryPool* FromPool::pool = new MemoryPool();
+
+//========================================================================
+
+#endif // MEMORYPOOL_H_INCLUDED
